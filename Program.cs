723f26e7d0efefee8eec3db36df704428c12251e
Ashwini <salunkheashwini1998@gmail.com>
2023-01-11 16:27:02 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@
             obj.push(30);
             obj.push(56);
             Console.WriteLine();
+            Console.WriteLine("Item popped from stack = " + obj.pop());
+
 
             Console.WriteLine("Top Element of the stack is : " + obj.peek());
 

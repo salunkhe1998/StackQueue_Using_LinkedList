cbce2da434a2bc260cf97d0e9994887789be785e
Ashwini <salunkheashwini1998@gmail.com>
2023-01-11 16:51:11 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StackQueueUsingLinkedList
+﻿using System.Threading.Tasks;
+
+namespace StackQueueUsingLinkedList
 {
     public class Program
     {
@@ -6,17 +8,28 @@
         {
             Console.WriteLine("***** Welocome to Stack & Queue Programs Using Linked list *****");
 
-            Stack obj = new Stack();
+            Queue obj = new Queue();
             Console.WriteLine("After push the elements are : ");
 
-            obj.push(70);
-            obj.push(30);
-            obj.push(56);
+            obj.enqueue(56);
+            obj.enqueue(30);
+            obj.enqueue(70);
             Console.WriteLine();
-            Console.WriteLine("Item popped from stack = " + obj.pop());
+            //Console.WriteLine("Item popped from Queue = " + obj.dequeue());
 
 
-            Console.WriteLine("Top Element of the stack is : " + obj.peek());
+            Console.WriteLine("Top Element of the Queue is : " + obj.peek());
+
+            //We can check other functios:
+
+
+            //Console.WriteLine();
+            //Console.WriteLine("size : " + obj.size());
+            //Console.WriteLine("peek : " + obj.peek());
+            //Console.WriteLine("dequeue : " + obj.dequeue());
+            //Console.WriteLine("size : " + obj.size());
+            //Console.WriteLine("peek : " + obj.peek());
+            //Console.WriteLine("isEmpty : " + obj.isEmpty());
 
         }
     }

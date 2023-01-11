3f44c472eeb2ba68de6ce9d5f0c0c97f37ee872b
Ashwini <salunkheashwini1998@gmail.com>
2023-01-11 15:34:34 +0530

    this is file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StackQueueUsingLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welocome to Stack & Queue Programs Using Linked list *****");
+
+        }
+    }
+}

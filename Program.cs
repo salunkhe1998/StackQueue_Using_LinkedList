1b80aca1520b09dbc11e4b450f31c152d53ae68c
Ashwini <salunkheashwini1998@gmail.com>
2023-01-11 15:47:25 +0530

    this is file

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@
         {
             Console.WriteLine("***** Welocome to Stack & Queue Programs Using Linked list *****");
 
+            Stack obj = new Stack();
+            Console.WriteLine("After push the elements are : ");
+
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            Console.WriteLine();
+
+            Console.WriteLine("Top Element of the stack is : " + obj.peek());
+
         }
     }
 }

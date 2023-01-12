ee49503f14dc7a8fa714a15d6c39ddcc9cf68640
Ashwini <salunkheashwini1998@gmail.com>
2023-01-12 15:26:13 +0530

    variable initillization

--- Node.cs
+++ Node.cs
@@ -10,9 +10,10 @@ namespace StackQueueUsingLinkedList
     {
         public int data;
         public NewNode next;
-        public NewNode(int data)
+        public NewNode(int value)
         {
-            this.data = data;
+            this.data = value;
+            this.next = null;
         }
     }
 }
